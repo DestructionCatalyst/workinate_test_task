1bd093ed6e711ce91c24c206611da0c4a628ee54
vladislav <komza.vlad@ya.ru>
2021-11-14 09:45:35 +0000

    Add Google search api

--- search/views.py
+++ search/views.py
@@ -1,5 +1,7 @@
 from django.http.response import JsonResponse, Http404
 from django.views.decorators.http import require_GET
+from search.google.google_request import google_search_api_request
+from search.google.google_response_formatter import GoogleResponseFormatter
 from django.http import HttpRequest
 
 
@@ -10,7 +12,8 @@ def read_get_param(request: HttpRequest, param: str):
     :param request: request to read parameter value from
     :param param: name of the parameter
     :return: value of the specified parameter
-    :raise django.http.Http404: if a request does not contain a parameter with the specified name
+    :raise django.http.Http404: if a request does not contain a parameter
+    with the specified name
     """
     query = request.GET.get(param)
     if query is None:
@@ -26,4 +29,7 @@ def search_yandex(request):
 @require_GET
 def search_google(request):
     query = read_get_param(request, 'query')
-    return JsonResponse({"data": [query]})
+    google_response = google_search_api_request(query)
+    formatted_response = \
+        GoogleResponseFormatter(google_response).get_formatted_response()
+    return JsonResponse({"data": formatted_response})
