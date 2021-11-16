c95ee96db02bbef1605147a758fe7339da737d9d
vladislav <komza.vlad@ya.ru>
2021-11-16 00:14:51 +0000

    Unified exceptions part 2

--- search/serpapi_yandex/exceptions.py
+++
@@ -1,2 +0,0 @@
-class SerpapiException(Exception):
-    pass
--- search/yandex/exceptions.py
+++
@@ -1,4 +0,0 @@
-class YandexException(Exception):
-    def __init__(self, message, code):
-        super().__init__(message)
-        self.code = code
